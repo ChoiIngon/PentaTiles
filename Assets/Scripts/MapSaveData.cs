5b69b826ee26e58ef931b537c905dab271e7ec7d
kukuta <kukuta@gmail.com>
2017-04-09 19:18:23 +0900

    에디터 모드에서 어셋 클릭시 자동으로 로딩

--- Assets/Scripts/MapSaveData.cs
+++ Assets/Scripts/MapSaveData.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine.SceneManagement;
+#endif
 [System.Serializable]
 public class MapSaveData : ScriptableObject {
 	public int stage;
@@ -14,4 +18,37 @@ public class MapSaveData : ScriptableObject {
 
     public float blockSlotScale;
 	public BlockSaveData[] blocks;
+
+	#if UNITY_EDITOR
+	[OnOpenAssetAttribute(1)]
+	public static bool Open(int instanceID, int line)
+	{
+		if(true == Application.isPlaying && true == Map.Instance.editMode)
+		{
+			string name = EditorUtility.InstanceIDToObject(instanceID).name;
+			MapSaveData data = Resources.Load<MapSaveData>(name);
+			if (null == data) {
+				return false;
+			}
+			Editor.Instance.Init();
+			Map.Instance.Init(data);
+			Editor.Instance.stage = data.stage;
+			Editor.Instance.level = data.level;
+			Editor.Instance.width = data.width;
+			Editor.Instance.height = data.height;
+			Editor.Instance.blockSlotScale = data.blockSlotScale;
+			Editor.Instance.blockSlotScale = Map.Instance.blockSlotScale;
+			for (int i = 0; i < Map.Instance.blocks.childCount; i++)
+			{
+				Block block = Map.Instance.blocks.GetChild(i).GetComponent<Block>();
+				Editor.Instance.blocks.Add(block.id, block);
+				Editor.Instance.blockID = Mathf.Max (Editor.Instance.blockID, block.id+1);
+			}
+
+			Debug.Log("success to load map file from " + "\'Assets/Resources/" + data.stage + "_" + data.level + ".asset" + "\'");
+			return true;
+		}
+		return false;
+	}
+	#endif
 }
