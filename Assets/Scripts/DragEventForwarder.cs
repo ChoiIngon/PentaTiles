f904d5885e86368e49e855c22b15f5b622759f90
ChoiIngon <kukuta@gmail.com>
2017-04-10 16:22:25 +0900

    스크롤 렉트 - 스티키 스냅 기능 추가
    하지만 하이어러키를 바꿔서 데이터가 로딩이 안된다

---
+++ Assets/Scripts/DragEventForwarder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragEventForwarder : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    ScrollSnapRect scrollRectSnap;
+
+    private void Start()
+    {
+        scrollRectSnap = GetComponentInParent<ScrollSnapRect>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(null != scrollRectSnap)
+        {
+            scrollRectSnap.OnBeginDrag(eventData);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (null != scrollRectSnap)
+        {
+            scrollRectSnap.OnDrag(eventData);
+        }
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (null != scrollRectSnap)
+        {
+            scrollRectSnap.OnEndDrag(eventData);
+        }
+    }
+}
